2a2236d77a1496c31fc936fa6dbb99364b20b7ed
Mark Napolitano <156marknap@gmail.com>
2023-04-27 16:22:00 +0800

    Set up backend files, should be converted fully to GraphQL API

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,31 +1,47 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-    type Query {
-        me: User
-    }
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
 
-    input SaveBookInput{
-        bookId: ID
-        title: String
-        authors: [String]
-        description: String
-        image: String
-        link: String
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: SaveBookInput): User
-        removeBook(bookId: ID!): User
-    }
+  type Query {
+    me: User
+  }
 
-`
+  input SaveBookInput {
+    bookId: String
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SaveBookInput): User
+    removeBook(bookId: String!): User
+  }
+`;
 
 module.exports = typeDefs;
